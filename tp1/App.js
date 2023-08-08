1c58f8b065e6bf833008f251f8755789f889e18b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 09:10:08 -0300

    PROBLEMAS CON EL BOTON

--- tp1/App.js
+++ tp1/App.js
@@ -1,31 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import {  Pressable, Text, View , TouchableOpacity, Button} from 'react-native';
-import  appStyles  from './styles.js';
+import { Pressable, Text, View, TouchableOpacity, Button, SafeAreaView, TextInput } from 'react-native';
+import appStyles from './styles.js';
+import { useState } from 'react'; 
 
 export default function App() {
-  
   const [nombre, setNombre] = useState('');
   const [telefono, setTelefono] = useState('');
   const [email, setEmail] = useState('');
   const [clave, setClave] = useState('');
+
   const handlePress = () => {
-    console.log('Nombre: '+ nombre);
-    console.log('Telefono: '+ telefono);
-    console.log('Email: '+ email);
-    console.log('Clave: '+ clave);
+    console.log('Nombre: ' + nombre);
+    console.log('Telefono: ' + telefono);
+    console.log('Email: ' + email);
+    console.log('Clave: ' + clave);
   };
+
   return (
-    <View style={appStyles.container}>
-      <Pressable onPress={handlePress} style={styles.button}>
-      <Text style={styles.buttonText}>Pressable</Text>
-    </Pressable>
-    <TouchableOpacity onPress={handlePress} style={styles.button}>
-      <Text style={styles.buttonText}>TouchableOpacity</Text>
-    </TouchableOpacity>
-    <Button title="Button"    onPress={handlePress} />
-      <StatusBar style="auto " />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={appStyles.container}>
+        <Text style={appStyles.textLabel}>Carga tus datos</Text>
+
+        <View style={appStyles.inputContainer}>
+          <Text style={appStyles.inputLabel}>Nombre</Text>
+          <TextInput
+            style={appStyles.input}
+            placeholder="ingrese..."
+            onChangeText={setNombre}
+            value={nombre}
+          />
+        </View>
+        <Text> {'\n'} </Text>
+
+        <View style={appStyles.inputContainer}>
+          <Text style={appStyles.inputLabel}>Telefono</Text>
+          <TextInput
+            style={appStyles.input}
+            placeholder="ingrese..."
+            onChangeText={setTelefono}
+            value={telefono}
+          />
+        </View>
+        <Text> {'\n'}</Text>
+        <View style={appStyles.inputContainer}>
+          <Text style={appStyles.inputLabel}>Email</Text>
+          <TextInput
+            style={appStyles.input}
+            placeholder="ingrese..."
+            onChangeText={setEmail}
+            value={email}
+          />
+        </View>
+        <Text> {'\n'} </Text>
+        <View style={appStyles.inputContainer}>
+          <Text style={appStyles.inputLabel}>Clave</Text>
+          <TextInput
+            style={appStyles.input}
+            placeholder="ingrese..."
+            onChangeText={setClave}
+            value={clave}
+          />
+        </View>
+        <Text> {'\n'} </Text>
+        <View style={appStyles.posBotton}>
+
+        <View  style={{flex: 2}}> 
+        <Pressable onPress={handlePress} style={appStyles.buttonText}>
+          <Text >Pressable</Text>
+        </Pressable>
+        </View>
+
+
+        <View  style={{flex: 2}}> 
+        <TouchableOpacity onPress={handlePress} style={appStyles.buttonText}>
+          <Text >TouchableOpacity</Text>
+        </TouchableOpacity>
+        </View>
+
+        
+        <View style={{ flex: 2 }}>
+            <View style={appStyles.buttonText}>
+              <Button title="Button" onPress={handlePress} />
+            </View>
+          </View>
+
+
+
+
+
+
+          
+        </View>
+
+
+
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
-
-
--- tp1/styles.js
+++ tp1/styles.js
@@ -1,17 +1,32 @@
-const appStyles = StyleSheet.create({
+const appStyles = {
     container: {
       flex: 1,
       backgroundColor: '#fff',
+      margin: 50,
       alignItems: 'center',
-      justifyContent: 'center',
+      //justifyContent: 'center',
     },
-    button: {
-        backgroundColor: '#3498db',
+    inputLabel: {
+      textAlign: 'left',
+    },
+    buttonText: {
+        backgroundColor: '#47F800',
         paddingVertical: 12,
         paddingHorizontal: 24,
         borderRadius: 5,
+        height: 40,
+        width: 220,
+        textAlign: 'left',
+
       },
-  });
+      input: {    height: 40, borderWidth: 2, padding: 1,  width: 200,},
+      posBotton: {
+        flexDirection     : 'col',        
+        textAlign         : 'center',        
+        textAlignVertical : 'auto',        
+        flex              : 1,
+      },
+  };
   
 
   export default appStyles;
